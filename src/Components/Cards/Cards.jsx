8c171ce82817e587a230015bbd843d01f646bea8
killshotxd <hassanansari211@gmail.com>
2022-12-19 09:53:16 +0530

    swapped LINK with useNavigate

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 const Cards = () => {
+  const navigate = useNavigate();
   const API = "https://webcom-api.vercel.app/";
 
   const [post, setPost] = useState([]);
@@ -19,23 +20,24 @@ const Cards = () => {
         return (
           <>
             <div className="container" style={{ height: "100vh" }}>
-              <Link key={posts.ID} to={`/posts/${posts.ID}`} state={{ posts }}>
-                <div
-                  key={posts.ID}
-                  style={{ width: "12rem" }}
-                  className="card p-2 mt-4"
-                >
-                  <img
-                    className=""
-                    style={{ height: "15rem", width: "auto" }}
-                    src={posts.COVER}
-                    alt=""
-                  />
-                  <p className="text-center mt-2">
-                    {posts.TITLE} ({posts.YEAR})
-                  </p>
-                </div>
-              </Link>
+              <div
+                onClick={() => {
+                  navigate(`/posts/${posts.ID}`, { state: { posts } });
+                }}
+                key={posts.ID}
+                style={{ width: "12rem", cursor: "pointer" }}
+                className="card p-2 mt-4"
+              >
+                <img
+                  className=""
+                  style={{ height: "15rem", width: "auto" }}
+                  src={posts.COVER}
+                  alt=""
+                />
+                <p className="text-center mt-2">
+                  {posts.TITLE} ({posts.YEAR})
+                </p>
+              </div>
             </div>
           </>
         );
